b9ffe68500a46c85cdfa1d2754a25643e066179c
Li Yunfei <2104857437@qq.com>
2026-09-17 22:26:04 +0800

    Step 25: run_diloco_round

--- model.py
+++ model.py
@@ -353,8 +353,46 @@ def compute_outer_gradient(global_params, worker_params_list):
 
     return outer_grad
 
-# Step 25 - run_diloco_round (not yet solved)
-# TODO: implement
+# Step 25 - run_diloco_round
+def run_diloco_round(global_params, outer_state, worker_shards, num_inner_steps, batch_size, inner_hparams, outer_lr, momentum_coef, seed):
+    worker_param_list=[]
+    worker_losses=[]
+
+    for i , (x_shard,y_shard) in enumerate(worker_shards):
+        worker_params, mean_loss = inner_train_worker(
+            params=global_params,
+            x_shard=x_shard,
+            y_shard=y_shard,
+            num_inner_steps=num_inner_steps,
+            batch_size=batch_size,
+            lr=inner_hparams["lr"],
+            beta1=inner_hparams["beta1"],
+            beta2=inner_hparams["beta2"],
+            eps=inner_hparams["eps"],
+            weight_decay=inner_hparams["weight_decay"],
+            seed=seed + i
+        )
+        worker_param_list.append(worker_params)
+        worker_losses.append(mean_loss)
+
+    
+    outer_grad=compute_outer_gradient(global_params, worker_param_list)
+    
+    outer_state = update_outer_momentum(
+        outer_state,
+        outer_grad,
+        momentum_coef
+    )
+
+    # 4. Nesterov 更新全局模型
+    new_global_params = nesterov_param_update(
+        global_params,
+        outer_state["momentum"],
+        outer_grad,
+        outer_lr,
+        momentum_coef
+    )
+    return new_global_params, outer_state, worker_losses
 
 # Step 26 - train_diloco (not yet solved)
 # TODO: implement
