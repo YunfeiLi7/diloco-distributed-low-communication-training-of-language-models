13a7edd59d64f55c29854df70a32e22a16939345
Li Yunfei <2104857437@qq.com>
2026-09-18 09:06:23 +0800

    Step 20: inner_train_worker

--- model.py
+++ model.py
@@ -305,7 +305,10 @@ def inner_train_worker(params, x_shard, y_shard, num_inner_steps, batch_size, lr
         x_batch,y_batch=sample_worker_batch(x_shard=x_shard, y_shard=y_shard, batch_size=batch_size, rng=rng)
         worker_params , adam_state ,loss=local_train_step(params=worker_params, adam_state=adam_state, x_batch=x_batch, y_batch=y_batch, lr=lr, beta1=beta1, beta2=beta2, eps=eps, weight_decay=weight_decay)
         mean_loss+=loss
-    mean_loss/=num_inner_steps
+    if num_inner_steps==0:
+        mean_loss=0
+    else:
+        mean_loss/=num_inner_steps
     return worker_params , mean_loss
 
 # Step 21 - init_outer_optimizer
